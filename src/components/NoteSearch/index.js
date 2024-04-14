8f6f64ada2517bf4d63db8f47b9acc10845026d1
duban-sg <universidad.duban@gmail.com>
2024-04-13 20:14:59 -0500

    first commit

---
+++ src/components/NoteSearch/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Notesearch({searchValue,setSearchValue}) {
+
+  return (
+    <input
+      placeholder="Buscar"
+      className='Search'
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+    
+  );
+}
+
+export { Notesearch };
---
+++ src/utils/settings.js
@@ -0,0 +1,4 @@
+
+export default {
+    REACT_APP_URL_API_SERVER: process.env.REACT_APP_URL_API_SERVER,
+};
